011bdf4eb23503505758a019a0b592b1844d1085
babaqulu <baba.qulu97@gmail.com>
2023-08-22 02:24:06 +0400

    'Updated'

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Sidebar.css'
+
+import { ReactComponent as Logo } from '../../img/icons/logo.svg'
+import { ReactComponent as Id } from '../../img/icons/id.svg'
+import { ReactComponent as Cube } from '../../img/icons/cube.svg'
+import { ReactComponent as Calculator } from '../../img/icons/calculater.svg'
+import { ReactComponent as Certificate } from '../../img/icons/certificate.svg'
+import { ReactComponent as Listdata } from '../../img/icons/listdata.svg'
+import { ReactComponent as Graph } from '../../img/icons/graph.svg'
+import { ReactComponent as Chart } from '../../img/icons/chart.svg'
+import { ReactComponent as Shield } from '../../img/icons/shield.svg'
+import { ReactComponent as Calendar } from '../../img/icons/calendar.svg'
+import { ReactComponent as Bank } from '../../img/icons/bank.svg'
+
+
+export default function Sidebar() {
+  return (
+    <div className='sidebarContainer'>
+      {/* <Logo className='logo' />
+      <div className='menulist'>
+      <Id />
+      <Cube />
+      <Calculator />
+      <Certificate />
+      <Listdata />
+      <Graph />
+      <Chart />
+      <Shield />
+      <Calendar />
+      <Bank />
+      </div> */}
+    </div>
+  )
+}
